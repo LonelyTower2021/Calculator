8743ed0fdc61acd8d7ef6097f1bd42e73e212f03
Alonelytower <lonelytower2021@gmail.com>
2022-01-17 12:32:07 -0800

    Organize logic into separate functions.

--- script.js
+++ script.js
@@ -1,56 +1,47 @@
-function add(a, b) {
-    return a + b;
-}
-
-function subtract(a, b) {
-    return a - b;
-}
-
-function multiply(a, b) {
-    return a * b;
-}
-
-function divide(a, b) {
-    return a / b;
-}
-
-function operate(operator, a, b) {
-    if (operator === "+") {return add(a, b);}
-    else if (operator === "-") {return subtract(a, b);}
-    else if (operator === "*") {return multiply(a, b);}
-    else if (operator === "/") {return divide(a, b);}
-    else {return NaN}
-}
-
 let userInput = new Array();
-let buttonNumMap = new Map([
-    ["button_7", 7],
-    ["button_8", 8],
-    ["button_9", 9],
-    ["button_4", 4],
-    ["button_5", 5],
-    ["button_6", 6],
-    ["button_1", 1],
-    ["button_2", 2],
-    ["button_3", 3],
-    ["button_0", 0],
-]);
 
-let buttonOperMap = new Map([
-    ["button_(", "("],
-    ["button_)", ")"],
-    ["button_/", "/"],
-    ["button_*", "*"],
-    ["button_-", "-"],
-    ["button_.", "."],
-    ["button_+", "+"],
-]);
+function equalsOperator(e) {
 
-let buttonFuncMap = new Map([
-    ["button_C", "C"],
-    ["button_B", "B"],
-    ["button_=", "="],
-]);
+    let processedInput = parseUserInput();
+
+    let total = processedInput.shift();
+    while (processedInput.length !== 0) {
+        let operator = processedInput.shift();
+        let b = processedInput.shift();
+        total = operate(operator, total, b);
+    }
+    
+    updateUserInput(total);
+    updateDisplay();
+}
+
+function parseUserInput() {
+    let processedInput = new Array();
+    let inputLength = userInput.length;
+    let number = 0;
+    for (let index = 0; index < inputLength; index++) {
+        let value = userInput[index];
+        if (!(isNaN(value)) && (index !== inputLength - 1)) {
+            number *= 10;
+            number += value;
+        } else if (!(isNaN(value))) {
+            number *= 10;
+            number += value;
+            processedInput.push(number);
+            number = 0;
+        } else {
+            processedInput.push(number);
+            processedInput.push(value);
+            number = 0;
+        }
+    }
+    return processedInput;
+}
+
+function updateUserInput(total) {
+    userInput = total.toString().split('').map(strNum => parseInt(strNum));
+    updateDisplay();
+}
 
 function appendNumButton(e) {
     userInput.push(buttonNumMap.get(e.target.id));
@@ -77,40 +68,60 @@ function popDisplay(e) {
     updateDisplay();
 }
 
-function equalsOperator(e) {
-    let processed = new Array();
-    let number = 0;
-
-    for (let index = 0; index < userInput.length; index++) {
-        let value = userInput[index];
-        if (!(isNaN(value)) && (index !== userInput.length - 1)) {
-            number *= 10;
-            number += value;
-        } else if (!(isNaN(value))) {
-            number *= 10;
-            number += value;
-            processed.push(number);
-            number = 0;
-        } else {
-            processed.push(number);
-            processed.push(value);
-            number = 0;
-        }
-    }
-
-    let total = processed.shift();
-    while (processed.length !== 0) {
-        let operator = processed.shift();
-        let b = processed.shift();
-        total = operate(operator, total, b);
-    }
-
-    userInput = total.toString().split('');
-    updateDisplay();
+function add(a, b) {
+    return a + b;
 }
 
-let buttons = document.querySelectorAll('.button');
+function subtract(a, b) {
+    return a - b;
+}
 
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operator, a, b) {
+    if (operator === "+") {return add(a, b);}
+    else if (operator === "-") {return subtract(a, b);}
+    else if (operator === "*") {return multiply(a, b);}
+    else if (operator === "/") {return divide(a, b);}
+    else {return NaN}
+}
+
+let buttonNumMap = new Map([
+    ["button_0", 0],
+    ["button_1", 1],
+    ["button_2", 2],
+    ["button_3", 3],
+    ["button_4", 4],
+    ["button_5", 5],
+    ["button_6", 6],
+    ["button_7", 7],
+    ["button_8", 8],
+    ["button_9", 9],
+]);
+
+let buttonOperMap = new Map([
+    // ["button_(", "("],
+    // ["button_)", ")"],
+    ["button_/", "/"],
+    ["button_*", "*"],
+    ["button_-", "-"],
+    // ["button_.", "."],
+    ["button_+", "+"],
+]);
+
+let buttonFuncMap = new Map([
+    ["button_C", "C"],
+    ["button_B", "B"],
+    ["button_=", "="],
+]);
+
+let buttons = document.querySelectorAll('.button');
 buttons.forEach(button => {
     if (buttonNumMap.has(button.id)) {
         button.addEventListener('click', appendNumButton);
